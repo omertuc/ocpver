1e75dd0ffa5c2b655ba4fa32914b74249f12d2e7
Omer Tuchfeld <omer@tuchfeld.dev>
2022-08-04 17:25:00 +0200

    Kernel version

--- ocp-to-rhcos.sh
+++ ocp-to-rhcos.sh
@@ -8,13 +8,30 @@ REPO=${REPO:-quay.io/openshift-release-dev/ocp-release}
 ARCH=${ARCH:-x86_64}
 
 MIN_Z=${MIN_Z:-1}
-MAX_Z=${MAX_Z:-26}
+MAX_Z=${MAX_Z:-3}
 
 PULL_SECRET_FILE=${PULL_SECRET:-"$HOME"/.docker/config.json}
 
+echo '['
 for Z in $(seq "$MIN_Z" "$MAX_Z"); do
-	oc adm --registry-config="$PULL_SECRET_FILE" release info "$REPO":"$MAJ_MINOR"."$Z"-"$ARCH" |
-		grep "machine-os " |
-		cut -d' ' -f4 |
-		awk '{printf "{\"'"$MAJ_MINOR.$Z"'\": \"" $1 "\"}\n"}'
+    OCP_VER="$MAJ_MINOR"."$Z"
+    RHCOS_VER=$(oc adm --registry-config="$PULL_SECRET_FILE" release info "$REPO":"$OCP_VER"-"$ARCH" | \
+		grep "machine-os " | \
+		cut -d' ' -f4)
+
+    KERNEL_VER=$(curl -sL https://releases-rhcos-art.cloud.privileged.psi.redhat.com/storage/releases/rhcos-4.10/410.84.202202251620-0/x86_64/commitmeta.json | jq '."ostree.linux"' -r)
+
+    FINAL=$(jq -n -c --arg ocp_ver "$OCP_VER" --arg rhcos_ver "$RHCOS_VER" --arg kernel_ver "$KERNEL_VER" '
+        {
+            "ocp_version": $ocp_ver,
+            "rhcos_ver": $rhcos_ver,
+            "kernel_ver": $kernel_ver
+        }')
+
+    if [[ "$Z" == "$MAX_Z" ]]; then
+        printf '\t%s\n' "$FINAL"
+    else
+        printf '\t%s,\n' "$FINAL"
+    fi
 done
+echo ']'
